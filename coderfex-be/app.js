1710631da1cc39f43f0727891ad398cfbede409b
MarcusNguyen11 <tuan.nv1495@gmail.com>
2023-11-28 12:09:41 +0700

    first commit

--- coderfex-be/app.js
+++ coderfex-be/app.js
@@ -10,7 +10,7 @@ require("dotenv").config();
 
 const cors = require("cors");
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: "https://luminous-dasik-4e94b6.netlify.app",
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
